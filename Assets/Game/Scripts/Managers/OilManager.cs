0bd2e6b9a464d46537a658ff104d368abca27fc4
Rafael Leão <rafaelbleaob@gmail.com>
2025-08-02 14:45:50 -0300

    New Oil Manager

--- Assets/Game/Scripts/Managers/OilManager.cs
+++ Assets/Game/Scripts/Managers/OilManager.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 public class OilManager : MonoBehaviour
 {
     [Header("Oil Dispenser")]
-    [SerializeField] private OilDispenser[] oilDispensers;
+    [SerializeField] private GameObject[] dispensedObjects;
+    [SerializeField] private GameObject[] oilBubbles;
     private const float TimeTillDispenseOil = 1.2f;
     private const float TimeOilDispensing = 3f;
     private const float DurationTime = 0.8f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        foreach (OilDispenser oilDispenser in oilDispensers)
+        for (var i = 0;  i < dispensedObjects.Length; i++)
         {
             Sequence mySequence = DOTween.Sequence();
-            mySequence.Append(oilDispenser.dispensedObject.transform.DOMoveY(0f, DurationTime).SetDelay(Random.Range(0.8f, 1.5f)).SetEase(Ease.Linear))
+            mySequence
+                // .OnStart(ActivateBubble(i))
+                .SetDelay(i * 0.8f + Random.Range(0, 0.5f))
+                .Append(dispensedObjects[i].transform.DOMoveY(0f, DurationTime)
+                    .SetDelay(0.7f)
+                    .SetEase(Ease.Linear))
+                    // .OnStart(DeactivateBubble(i)))
                 .Append(
-                    oilDispenser.dispensedObject.transform.DOMoveY(-18f, DurationTime).SetDelay(TimeOilDispensing).SetEase(Ease.Linear))
+                    dispensedObjects[i].transform.DOMoveY(-18f, DurationTime).SetDelay(TimeOilDispensing)
+                        .SetEase(Ease.Linear))
                 .SetDelay(TimeTillDispenseOil);
             mySequence.SetLoops(-1, LoopType.Restart);
         }
+    }
+
+    private TweenCallback ActivateBubble(int pos)
+    {
+        Debug.Log("oi");
+        oilBubbles[pos].SetActive(true);
+        
+        return null;
+    }
+    
+    private TweenCallback DeactivateBubble(int pos)
+    {
+        Debug.Log("ola");
+        oilBubbles[pos].SetActive(false);
+        
+        return null;
     }
 }
