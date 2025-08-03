258fe57197a4ea3427b086f7f6b517b62862427c
Rafael Leão <rafaelbleaob@gmail.com>
2025-08-02 21:08:04 -0300

    Conveyor working and DeathArea ignoring Dummy

--- Assets/Game/Scripts/ConveyorBelt.cs
+++ Assets/Game/Scripts/ConveyorBelt.cs
@@ -1,40 +1,91 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
-public class ConveyorBelt : MonoBehaviour
+namespace Game.Scripts
 {
-    public bool isOn = true;
-    public bool clockwise = true;
-    public float speed = 500;
-
-    private void OnCollisionStay2D(Collision2D collision)
+    public class ConveyorBelt : MonoBehaviour
     {
-        if (isOn)
+        [SerializeField] private GameObject[] friedChickens;
+    
+        public bool isOn = true;
+        public bool clockwise = true;
+        public bool hasInfiniteChicken;
+        public float speed = 500;
+
+        private void Start()
         {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Players") ||
-                collision.gameObject.layer == LayerMask.NameToLayer("InteractableObjects"))
+            DeathArea.OnDeathArea += ResetChickens;
+            
+            if (hasInfiniteChicken)
+            {
+                StartCoroutine(InfiniteChicken());
+            }
+        }
+    
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            if (isOn)
             {
                 var rb = collision.gameObject.GetComponent<Rigidbody2D>();
-                
-                if (clockwise)
+            
+                if (collision.gameObject.layer == LayerMask.NameToLayer("Players"))
                 {
-                    rb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+                    if (clockwise)
+                    {
+                        rb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+                    }
+                    else
+                    {
+                        rb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+                    }
                 }
-                else
+                else if (collision.gameObject.layer == LayerMask.NameToLayer("InteractableObjects"))
                 {
-                    rb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+                    if (clockwise)
+                    {
+                        rb.AddForce(Vector2.left * speed / 20, ForceMode2D.Force);
+                    }
+                    else
+                    {
+                        rb.AddForce(Vector2.right * speed / 20, ForceMode2D.Force);
+                    }
                 }
             }
         }
-    }
 
-    public void ToggleOnOff()
-    {
-        isOn = !isOn;
-    }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer("InteractableObjects"))
+            {
+                // ResetChickens(collision.gameObject);
+                // StartCoroutine(InfiniteChicken());
+            }
+        }
+
+        private IEnumerator InfiniteChicken()
+        {
+            foreach (var friedChicken in friedChickens)
+            {
+                friedChicken.SetActive(true);
+                
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        private void ResetChickens(GameObject gameObject)
+        {
+            gameObject.transform.position = new Vector3(16, 0);
+            gameObject.SetActive(false);
+        }
+
+        public void ToggleOnOff()
+        {
+            isOn = !isOn;
+        }
     
-    public void ToggleDirection()
-    {
-        clockwise = !clockwise;
+        public void ToggleDirection()
+        {
+            clockwise = !clockwise;
+        }
     }
 }
--- Assets/Game/Scripts/DeathArea.cs
+++ Assets/Game/Scripts/DeathArea.cs
@@ -4,11 +4,18 @@ public class DeathArea : MonoBehaviour
 {
     [SerializeField] private string deathAreaSfxName;
     [SerializeField] private LayerMask interactableObjectsLayer;
+
+    public static event System.Action<GameObject> OnDeathArea;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerBase>().ModifyHealth(-1);
+        }
+        else if (other.CompareTag("Dummy"))
+        {
+            OnDeathArea?.Invoke(other.gameObject);
         }
         else if (((1 << other.gameObject.layer) & interactableObjectsLayer) != 0)
         {
